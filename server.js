e06c947585e578b25902c1657f4703d4e96195e4
Sophie Miller <millersg47@gmail.com>
2022-05-03 16:15:01 -0700

    adds path to notes.html on click of button

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
+const path = require('path');
 const express = require('express');
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
+
+//unsure if I need this
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+  
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
