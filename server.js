364bdb5b41885ff9d463b97840421516359a2c25
Sophie Miller <millersg47@gmail.com>
2022-05-03 16:46:05 -0700

    additional setup on server.js file

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.static('public'));
 //unsure if I need this
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
+//redirects to notes.html file on click
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
   
 
