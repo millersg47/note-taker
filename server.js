77c4a5dbce81e00b65f6527c9dbd4c744f795014
Sophie Miller <millersg47@gmail.com>
2022-05-03 15:36:00 -0700

    sets up server.js file with minimum requirements

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const fs = require('fs');
+const express = require('express');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
